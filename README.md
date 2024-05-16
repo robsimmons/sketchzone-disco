c310f2b92b19eb53af0d5b0440c61b0674d723ea
Rob Simmons <robsimmons@gmail.com>
2024-05-16 08:21:56 -0400

    Basicest site

---
+++ README.md
@@ -0,0 +1,3 @@
+# sketchzone on Disco
+
+Using [sketchzone](https://github.com/robsimmons/sketchzone/) as a static [Vite](https://vitejs.dev/-generated site on [Disco](https://letsdisco.dev/).
---
+++ index.tsx
@@ -0,0 +1,41 @@
+import { defaultKeymap, history, historyKeymap } from "@codemirror/commands";
+import { EditorView, keymap, lineNumbers } from "@codemirror/view";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { DOCUMENT, setup } from "sketchzone";
+
+function createAndMountInspector(root: HTMLDivElement, doc: DOCUMENT) {
+  const reactRoot = ReactDOM.createRoot(root);
+  reactRoot.render(
+    React.createElement(() => {
+      const [numPresses, setNumPresses] = React.useState(0);
+
+      return (
+        <div className="sketchzone-rounded-inspector">
+          <div>There is/are {doc.split("\n").length} line(s)</div>
+          <div>You've pushed the button {numPresses} time(s).</div>
+          <button onClick={() => setNumPresses(numPresses + 1)}>Press</button>
+        </div>
+      );
+    })
+  );
+}
+
+setup({
+  createAndMountInspector,
+  appName: "simple-disco",
+  infoUrl: "https://glitch.com/edit/#!/sketchzone-simple-react?path=index.jsx",
+  codemirrorExtensions: [
+    lineNumbers(),
+    history(),
+    EditorView.lineWrapping,
+    keymap.of([...defaultKeymap, ...historyKeymap]),
+  ],
+  defaultEntries: [
+    `Sketchzone on disco
+
+Kinda like any other static site on disco, I guess?`,
+    `Second tab`,
+    `Third tab`,
+  ],
+});
